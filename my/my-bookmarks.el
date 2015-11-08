46ff4a4432917ee68f11bd7f29684a087e5a4225
James Lee <james@substantial.com>
2015-11-08 13:40:43 -0800

    Add bookmarks

--- my/my-bookmarks.el
+++ my/my-bookmarks.el
@@ -2,7 +2,67 @@
 ;;; This format is meant to be slightly human-readable;
 ;;; nevertheless, you probably don't want to edit it.
 ;;; -*- End Of Bookmark File Format Version Stamp -*-
-((".emacs.d"
+(("booking"
+ (filename . "~/Projects/curadora/booking/")
+ (front-context-string . ".git\n  -rw-r--r-")
+ (rear-context-string . "10 Nov  5 15:33 ")
+ (position . 379))
+("juniper-api"
+ (filename . "~/Projects/curadora/juniper-api/")
+ (front-context-string . "NOTES.md\n  -rw-r")
+ (rear-context-string . "82 Nov  4 13:46 ")
+ (position . 496))
+("integration-tests"
+ (filename . "~/Projects/curadora/integration-tests/")
+ (front-context-string . ".git\n  -rw-r--r-")
+ (rear-context-string . "08 Sep 29 10:01 ")
+ (position . 253))
+("workers"
+ (filename . "~/Projects/curadora/workers/")
+ (front-context-string . ".git\n  -rw-r--r-")
+ (rear-context-string . "08 Sep  4 08:46 ")
+ (position . 242))
+("provider-adapter"
+ (filename . "~/Projects/curadora/provider-adapter/")
+ (front-context-string . ".git\n  -rw-r--r-")
+ (rear-context-string . "08 Sep  4 08:46 ")
+ (position . 251))
+("hotel-api"
+ (filename . "~/Projects/curadora/hotel-api/")
+ (front-context-string . ".git\n  -rw-r--r-")
+ (rear-context-string . "08 Sep  4 08:46 ")
+ (position . 241))
+("deployment"
+ (filename . "~/Projects/curadora/deployment/")
+ (front-context-string . ".bundle\n  drwxr-")
+ (rear-context-string . "02 Sep  4 08:46 ")
+ (position . 245))
+("cms"
+ (filename . "~/Projects/curadora/cms/")
+ (front-context-string . ".bundle\n  drwxr-")
+ (rear-context-string . "02 Sep  4 08:46 ")
+ (position . 238))
+("chef"
+ (filename . "~/Projects/curadora/chef/")
+ (front-context-string)
+ (rear-context-string . " 4 08:51 vendor\n")
+ (position . 1747))
+("booking-site"
+ (filename . "~/Projects/curadora/booking-site/")
+ (front-context-string . ".babelrc\n  -rw-r")
+ (rear-context-string . "74 Sep  4 08:46 ")
+ (position . 251))
+("curadora"
+ (filename . "~/Projects/curadora/")
+ (front-context-string . ".git\n  -rw-r--r-")
+ (rear-context-string . "42 Sep  4 09:25 ")
+ (position . 234))
+("mansion-global"
+ (filename . "~/Projects/mansion-global/")
+ (front-context-string . "app\n  drwxr-xr-x")
+ (rear-context-string . "76 Jan 23  2015 ")
+ (position . 1251))
+(".emacs.d"
  (filename . "~/.emacs.d/")
  (front-context-string . "ac-comphist.dat\n")
  (rear-context-string . " 6 Dec  5 11:01 ")
